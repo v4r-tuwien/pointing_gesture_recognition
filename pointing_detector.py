fix hardcoded depth encoding and mm m scaling

pointing_detector.py
@@ -46,6 +46,8 @@ class PointingDetector:
                 ):
 
         self.camera_info = np.asarray(rospy.get_param('/pose_estimator/intrinsics'))
+        self.depth_encoding = rospy.get_param('/pose_estimator/depth_encoding')
+        self.depth_scale = rospy.get_param('/pose_estimator/depth_scale')        
         self.frame_id = frame_id
         self.color_topic = color_topic
         self.depth_topic = depth_topic
@@ -169,8 +171,9 @@ class PointingDetector:
         depth = req.depth
 
         try:
-            # Convert ROS Image message to OpenCV image with 16UC1 encoding
-            depth_img = self.bridge.imgmsg_to_cv2(depth, "16UC1")
+            depth.encoding = self.depth_encoding
+            depth_img = CvBridge().imgmsg_to_cv2(depth, self.depth_encoding)
+            depth_img = depth_img/int(self.depth_scale)
         except CvBridgeError as e:
             rospy.logerr(f"Depth Image CvBridge Error: {e}")
 

utils.py
@@ -20,7 +20,7 @@ def get_depth_coordinates(x, y, depth_image, filtering=True, filter_width=3):
     # Filtering the depth values in the neighborhood
 
     # Unfiltered z-coordinate (depth) in meters
-    z = depth_image[y, x] / 1000
+    z = depth_image[y, x]
     
     if filtering:
         half = filter_width // 2
@@ -36,7 +36,7 @@ def get_depth_coordinates(x, y, depth_image, filtering=True, filter_width=3):
         # Return the filtered depth value of the pixel neighborhood
         if filtering:
             z_list = depth_image[y - half : y + half, x - half : x + half]
-            z = np.median(z_list) / 1000
+            z = np.median(z_list)
 
     if z <= 0:
         rospy.logwarn("Warning! Depth is zero!")